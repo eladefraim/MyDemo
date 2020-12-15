efc51a5097af4d8916ecfc52786ba78e6fc3fdd3
Elad Efraim <48961545+eladefraim@users.noreply.github.com>
2020-12-15 15:46:39 +0200

    Add files via upload

---
+++ Untitled1.js
@@ -0,0 +1,12 @@
+[
+  {
+    "name": "data from mock 1",
+    "age": 05,
+    "secretIdentity": "Dan Jukes"
+  },
+  {
+    "name": "data from mock 2",
+    "age": 06,
+    "secretIdentity": "Jane Wilson"
+  }
+]
---
+++ Untitled2.js
@@ -0,0 +1,12 @@
+[
+  {
+    "name": "data from backend 1",
+    "age": 05,
+    "secretIdentity": "Dan Jukes"
+  },
+  {
+    "name": "data from backend 2",
+    "age": 06,
+    "secretIdentity": "Jane Wilson"
+  }
+]
