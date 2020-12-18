a5c2b3d07f4c5c959f26c86e58c8d356259b5185
Elad Efraim <48961545+eladefraim@users.noreply.github.com>
2020-12-18 13:07:00 +0200

    Update Untitled2.js

--- Untitled2.js
+++ Untitled2.js
@@ -1,12 +1,10 @@
-[
-  {
-    "name": "data from backend 1",
-    "age": 05,
-    "secretIdentity": "Dan Jukes"
-  },
-  {
-    "name": "data from backend 2",
-    "age": 06,
-    "secretIdentity": "Jane Wilson"
-  }
-]
+var family = [{
+    "name" : "bob",
+    "age" : "12",
+    "gender" : "female"
+},
+{
+    "name" : "alice",
+    "age" : "10",
+    "gender" : "female"
+}];
